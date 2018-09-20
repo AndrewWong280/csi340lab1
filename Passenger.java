b06bd38b9f930c833c1b0fd0c0f92d5fc612aaea
ryanweeks <ryan.weeks@mymail.champlain.edu>
2018-09-20 16:31:36 -0400

    Added printItenerary()

--- Passenger.java
+++ Passenger.java
@@ -5,11 +5,39 @@ public class Passenger {
 	private ArrayList<Trip> trips = new ArrayList<Trip>();
 	private String name;
 
+	public Trip getTrip(int index) {
+		return trips.get(index);
+	}
+	
+	void updateTrip(int index, Trip single_trip) {
+		trips.set(index, single_trip);
+	}
+
+	public ArrayList<Trip> getTrips() {
+		return trips;
+	}
+
+	public void setTrips(ArrayList<Trip> trips) {
+		this.trips = trips;
+	}
+
 	public String getName() {
 		return name;
 	}
-
-	public Trip getTrip(int index) {
-		return trips.get(index);
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void printItenerary() {
+		System.out.print("Name        : " + name + "\n");
+		for(int i = 0; i < trips.size(); i++) {
+			System.out.print("From        : " + trips.get(i).getFlight().getOrigin() + "\n");
+			System.out.print("To          : " + trips.get(i).getFlight().getDestination() + "\n");
+			System.out.print("Date        : " + trips.get(i).getFlight().getDepartureDate() + "\n");
+			System.out.print("Time        : " + trips.get(i).getFlight().getDepartureTime() + "\n");
+			System.out.print("Seat Number : " + trips.get(i).getSeatNumber() + "\n");
+			System.out.print("Carrier     : " + trips.get(i).getFlight().getAircraft().getCarrier() + "\n");
+		}
 	}
 }
