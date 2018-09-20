d397eec3c00dce3d95a406fe25394985f887a02a
ryanweeks <ryan.weeks@mymail.champlain.edu>
2018-09-20 16:10:24 -0400

    Added boolean var: booked

--- Seat.java
+++ Seat.java
@@ -2,17 +2,34 @@
 public class Seat {
 	private SeatType type;
 	private String seatNumber;
-
+	private boolean booked;
+	
 	public Seat(SeatType type, String seatNumber) {
 		this.type = type;
 		this.seatNumber = seatNumber;
 	}
 
-	public SeatType getType() {
-		return type;
+	public String getSeatNumber() {
+		return seatNumber;
 	}
 
-	public String getNumber() {
-		return seatNumber;
+	public void setSeatNumber(String seatNumber) {
+		this.seatNumber = seatNumber;
+	}
+
+	public boolean isBooked() {
+		return booked;
+	}
+
+	public void setBooked(boolean booked) {
+		this.booked = booked;
+	}
+
+	public void setType(SeatType type) {
+		this.type = type;
+	}
+
+	public SeatType getType() {
+		return type;
 	}
 }
