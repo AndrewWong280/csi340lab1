cb8b188f7d6aadc938f64b165a750bad8b8aa16f
ashtonwallen <aashton482@gmail.com>
2018-09-21 09:26:59 -0400

    Fixed bug with assigning seat letters Added some code to test class

--- AirlineTester.java
+++ AirlineTester.java
@@ -4,6 +4,13 @@ public class AirlineTester
 {
     public static void main(String[] args)
     {
-        
+        AirlineCompany jetBlue = new AirlineCompany();
+        Airplane plane1 = new Airplane();
+        plane1.setAircraftType("Q400");
+        plane1.setTailNumber("N49AF");
+        plane1.setBusinessRows(20);
+        plane1.setFirstClassRows(10);
+        plane1.setSeatsPerRow(3);
+        plane1.setSeats(); 
     }
 }
