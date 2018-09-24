8fb1c183d6a64c54f1236bf7bf7c542c05e86a5e
ashtonwallen <aashton482@gmail.com>
2018-09-24 14:30:01 -0400

    Expanded test class, printItenerary works

--- AirlineTester.java
+++ AirlineTester.java
@@ -8,9 +8,20 @@ public class AirlineTester
         Airplane plane1 = new Airplane();
         plane1.setAircraftType("Q400");
         plane1.setTailNumber("N49AF");
+        plane1.setCarrier(jetBlue);
         plane1.setBusinessRows(20);
         plane1.setFirstClassRows(10);
         plane1.setSeatsPerRow(3);
         plane1.setSeats(); 
+        
+        Flight flight1 = new Flight("1044", "KBTV", "KBGR", "9/26/18", "15:30", plane1);
+        
+        Passenger passenger1 = new Passenger("Joe Bodega");
+        Trip trip1 = new Trip(flight1);
+        trip1.setSeat("2A", SeatType.VIP);
+        passenger1.addTrip(trip1);
+        
+        passenger1.printItenerary();
+        
     }
 }
