9b3a987794d68841f5c949b9dcd11e4ffc129b98
ashtonwallen <aashton482@gmail.com>
2018-09-24 18:23:40 -0400

    added getter/setter for AirlineCompany

--- AirlineCompany.java
+++ AirlineCompany.java
@@ -5,6 +5,13 @@ public class AirlineCompany
 	private ArrayList<Airplane> fleet;
 	private String name;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 	public AirlineCompany()
 	{
